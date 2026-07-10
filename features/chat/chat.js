b3eb4bf0f254f3c41ac20061ee75b72d4835c1c7
abhiravgotra92 <abhiravgotra92@gmail.com>
2026-07-10 11:21:01 -0300

    Update chat.js

--- features/chat/chat.js
+++ features/chat/chat.js
@@ -1506,7 +1506,7 @@ function setTheme(theme) {
 }
 
 (function initApp() {
-  const savedTheme = localStorage.getItem("Nut_theme") || "dark";
+  const savedTheme = localStorage.getItem("Nut_theme") || "win98";
   setTheme(savedTheme);
   // Admin credentials are hardcoded.
   // Username: 563, Password: 563
